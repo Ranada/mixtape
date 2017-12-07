e53b14ab9a8a34e871e44005a61181ca481fc5be
Neil Ranada <neil.ranada@gmail.com>
2017-12-07 01:39:43 -0800

    add click eventListener on the album cover to cycle through different albums.

--- scripts/album.js
+++ scripts/album.js
@@ -30,6 +30,22 @@
      ]
  };
 
+ // Neil's album
+ var albumNeil= {
+     title: 'Just Another Kook',
+     artist: 'Neil Ranada',
+     label: 'Indie',
+     year: '2017',
+     albumArtUrl: 'assets/images/album_covers/15.png',
+     songs: [
+         { title: 'Searching for the Green Flash', duration: '6:00' },
+         { title: 'Trapped in the Impact Zone', duration: '4:30' },
+         { title: 'Cruising on the Foam Top Express', duration: '5:25'},
+         { title: 'Shhh. Coffee First', duration: '3:15' },
+         { title: 'Dawn Patrol', duration: '2:45'}
+     ]
+ };
+
  var createSongRow = function(songNumber, songName, songLength) {
     var template =
        '<tr class="album-view-song-item">'
@@ -41,6 +57,10 @@
 
     return template;
 };
+
+// needed to copy albumImage so it could be access by event listner for changing albums
+var albumImage = document.getElementsByClassName('album-cover-art')[0];
+
 
 var setCurrentAlbum = function(album) {
     // #1
@@ -68,3 +88,15 @@ var setCurrentAlbum = function(album) {
 window.onload = function() {
     setCurrentAlbum(albumPicasso);
 };
+
+var albumsList = [albumPicasso, albumMarconi, albumNeil]; // Array of album objects
+var index = 0; // Use as starting point
+var currentAlbum = setCurrentAlbum(albumsList[index]); // Default is first index in albumsList array
+
+albumImage.addEventListener('click',function(){ // Click event function
+  index++; // Increase index by 1 to access next item in array
+  if (index == albumsList.length) { // If index is on the last item in the albumsList array
+      index = 0; // Go back to the first item in the albumsList array
+  }
+  setCurrentAlbum(albumsList[index]); // Call setCurrentAlbum function with next item in array
+})
